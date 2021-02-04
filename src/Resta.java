028e9c41ee2d06cd35288128bdad8256dbbef1f1
DeiMoS-es <nagib.86@gmail.com>
2021-02-04 12:47:08 +0100

    documento la clase Resta y añado los atributos

--- src/Resta.java
+++ src/Resta.java
@@ -1,4 +1,24 @@
+/**
+ * Esta clase representa la clase resta de una calculadora que realiza las 4 operaciones básicas
+ * @author Nagib Delgado
+ * @since 29/02/2021
+ * @version 0.1
+ * 
+ * 	Resta de dos números reales, tendrá 2 parámetros de entrada y uno de salida que será la solución.
+	Resta de dos números enteros, tendrá 2 parámetros de entrada y uno de salida que será la solución.
+	Resta de tres números reales, tendrá 3 parámetros de entrada y uno de salida que será la solución.
+	Resta con valor acumulado, tendrá un parámetro de entrada y la clase deberá de guardar el valor acumulado.
+
+ */
 
 public class Resta {
-
+	/**
+	 * Zona definicion de atributos
+	 * 
+	 */
+	private double n1;
+	private double n2;
+	private double n3;
+	private int n4;
+	private int acu =0;
 }
